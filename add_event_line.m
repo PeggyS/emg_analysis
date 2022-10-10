86b71198169442290451e5f051a6edb63cc1d988
PeggyS <peggy.skelly@gmail.com>
2022-10-10 16:18:12 -0400

    Add event lines to cocontraction app

--- add_event_line.m
+++
@@ -1,9 +0,0 @@
-function add_event_line(app, event_code, time)
-
-
-line(app.UIAxes_bicep,  [time time], 
-	line(app.UIAxes_tricep, 
-	line(app.UIAxes_cci, 
-	
-return
-end
--- emg_analysis_draw_events.m
+++ emg_analysis_draw_events.m
@@ -1,23 +1,41 @@
 function emg_analysis_draw_events(app)
 % draw the move, relax, and next events
 
-% delete existing event lines
-h_lines = findobj(app.CoConUIFigure, '-regexp', 'Tag', 'line_event.*');
-if ~isempty(h_lines)
-	delete(h_lines)
-end
-
-evt_code_list = {'move', 'relax', 'next', 'Viconstart'};
-evt_color_list = {[0 0 0.9], [0.8 0 0], [0.2 0.7 0.7], [0.5 0.5 0.5]};
-
+% delete existing event lines - were already deleted with the data lines
+% h_lines = findobj(app.CoConUIFigure, '-regexp', 'Tag', 'line_.*event.*');
+% if ~isempty(h_lines)
+% 	delete(h_lines)
+% end
 
 for e_cnt = 1:length(app.emg_data.event)
-	switch app.emg_data.event(e_cnt)
+	event_code = app.emg_data.event(e_cnt).code;
+	switch lower(event_code)
 		case 'move'
-			evt_
-			add_event_line(app, event_code, time)
-	
-	keyboard
+			event_color = [0 0 0.9];
+		case 'relax'
+			event_color = [0.8 0 0];
+		case 'next'
+			event_color = [0.2 0.7 0.7];
+		case 'viconstart'
+			event_color = [0.2 0.7 0.2];
+		case 'viconstop'
+			event_color = [0.9 0.2 0.2];
+		otherwise
+			event_color = [0 0 0];
+	end
+	% line in each axes 
+	time = app.emg_data.event(e_cnt).time;
+	h1 = line(app.UIAxes_bicep,  [time time], app.UIAxes_bicep.YLim, 'Color', event_color, ...
+		'LineWidth', 2, ...
+		'Tag', ['line_bicep_ax_event' num2str(e_cnt)]);
+	h2 = line(app.UIAxes_tricep,  [time time], app.UIAxes_tricep.YLim, 'Color', event_color, ...
+		'LineWidth', 2, ...
+		'Tag', ['line_tricep_ax_event' num2str(e_cnt)]);
+	h3 = line(app.UIAxes_cci,  [time time], app.UIAxes_cci.YLim, 'Color', event_color, ...
+		'LineWidth', 2, ...
+		'Tag', ['line_cci_ax_event' num2str(e_cnt)]);
+
+	app.links.event_lines(e_cnt) = linkprop([h1, h2, h3], 'XData');
 end % each event
 
 return
