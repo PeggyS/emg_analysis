5e0a113e5f06d2fb6a46bb27d8d714ee41b4705b
PeggyS <peggy.skelly@gmail.com>
2022-10-20 14:50:53 -0400

    Fix elbow angle error
    Use LElbAnglesXZY_X_deg NOT ...Z_deg

--- emg_analysis_draw_elbow_data.m
+++ emg_analysis_draw_elbow_data.m
@@ -1,10 +1,10 @@
 function emg_analysis_draw_elbow_data(app)
 
-% data = table2array(app.vicon_data.tbl(:, {'LElbAnglesXZY_Z_deg'}));
+% data = table2array(app.vicon_data.tbl(:, {'LElbAnglesXZY_X_deg'}));
 % line(app.UIAxes_elbow_angle, app.vicon_data.emg_time, data, ...
 % 	'Tag', 'line_elbow_angle_raw', 'Color', [0 0.4470 0.7410 0.25])
 
-data = table2array(app.vicon_data.lpfilt(:, {'LElbAnglesXZY_Z_deg'}));
+data = table2array(app.vicon_data.lpfilt(:, {'LElbAnglesXZY_X_deg'}));
 line(app.UIAxes_elbow_angle, app.vicon_data.emg_time, data, ...
 	'Tag', 'line_elbow_angle_lpfilt', 'Color', 'k')
 
