e2acde8b4d3b01e611bb6c8422287ca7fd274364
PeggyS <peggy.skelly@gmail.com>
2022-10-19 20:02:47 -0400

    Add computing and saving cocontraction info

--- compute_cocontraction_from_patches.m
+++ compute_cocontraction_from_patches.m
@@ -2,13 +2,21 @@ function compute_cocontraction_from_patches(app)
 
 app.cocontraction_data = struct;
 
+% elbow angle data
+h_elb_angle_line = findobj(app.UIAxes_elbow_angle, 'Tag', 'line_elbow_angle_lpfilt');
+
+
 % for each of the coactivation patches, compute stuff about it
 for p_cnt = 1:length(app.coactivation_patches)
-	app.cocontraction_data.begin_t
-	app.cocontraction_data.end_t
-	app.cocontraction_data.begin_angle
-	app.cocontraction_data.end_angle
-	app.cocontraction_data.cocontraction_auc
+	t_begin = min(app.coactivation_patches(p_cnt).Vertices(:,1));
+	t_end = max(app.coactivation_patches(p_cnt).Vertices(:,1));
+	app.cocontraction_data.begin_t(p_cnt) = t_begin;
+	app.cocontraction_data.end_t(p_cnt) = t_end;
+	ind_begin = find(h_elb_angle_line.XData >= t_begin, 1, 'first');
+	ind_end = find(h_elb_angle_line.XData >= t_end, 1, 'first');
+	app.cocontraction_data.begin_angle(p_cnt) = h_elb_angle_line.YData(ind_begin);
+	app.cocontraction_data.end_angle(p_cnt) = h_elb_angle_line.YData(ind_end);
+	app.cocontraction_data.cocontraction_auc(p_cnt) = compute_auc(app.coactivation_patches(p_cnt).Vertices);
 end
 
 return
---
+++ save_cocontraction_info.m
@@ -0,0 +1,37 @@
+function save_cocontraction_info(app)
+% get default save path
+save_path = get_save_path(app, true);
+if save_path==0
+	disp('No Cocontraction  info was saved')
+	return
+end
+
+
+% analyzer's initials and date
+analysis_by = upper(app.AnalysisbyEditField.Value);
+analysis_date = app.AnalysisdateEditField.Value;
+
+
+% add analysis info
+app.cocontraction_data.analysis_by = analysis_by;
+app.cocontraction_data.analysis_date = analysis_date;
+
+% file name
+[~,fname,~] = fileparts(app.FileNameLabel.Text);
+
+save_file = fullfile(save_path,  [fname '_cocontraction_info.txt']);
+
+if exist(save_file, 'file') == 2
+	selected_option = uiconfirm(app.CoConUIFigure, [save_file 'already exists. Do you want to overwrite it?'], ...
+		'Confirm Save', 'Options', {'Yes','No'});
+	if contains(selected_option, 'Yes')
+		% write the file
+		write_info(save_file, app.cocontraction_data)
+	end
+else
+	% write the file
+	write_info(save_file, app.cocontraction_data)
+end
+
+return
+end
