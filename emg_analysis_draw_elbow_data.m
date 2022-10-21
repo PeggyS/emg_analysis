5df808902df52973f6d4ca6eccb6d15dfe2af487
PeggyS <peggy.skelly@gmail.com>
2022-10-21 16:53:35 -0400

    Add provision for Left or Right elbow angles

--- emg_analysis_draw_elbow_data.m
+++ emg_analysis_draw_elbow_data.m
@@ -4,7 +4,14 @@ function emg_analysis_draw_elbow_data(app)
 % line(app.UIAxes_elbow_angle, app.vicon_data.emg_time, data, ...
 % 	'Tag', 'line_elbow_angle_raw', 'Color', [0 0.4470 0.7410 0.25])
 
-data = table2array(app.vicon_data.lpfilt(:, {'LElbAnglesXZY_X_deg'}));
+% is it left or right?
+if any(contains(app.vicon_data.lpfilt.Properties.VariableNames, 'RElb'))
+	angle_var = 'RElbAnglesXZY_X_deg';
+end
+if any(contains(app.vicon_data.lpfilt.Properties.VariableNames, 'LElb'))
+	angle_var = 'LElbAnglesXZY_X_deg';
+end
+data = table2array(app.vicon_data.lpfilt(:, {angle_var}));
 line(app.UIAxes_elbow_angle, app.vicon_data.emg_time, data, ...
 	'Tag', 'line_elbow_angle_lpfilt', 'Color', 'k')
 
