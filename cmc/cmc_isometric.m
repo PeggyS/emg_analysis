59de84f279628cf66a1e000ac234e51e2f60efb5
PeggyS <peggy.skelly@gmail.com>
2023-05-18 20:26:59 -0400

    Update cmc_isometric.m

--- cmc/cmc_isometric.m
+++ cmc/cmc_isometric.m
@@ -16,6 +16,7 @@ end
 % read in the data
 [pname, fname, ext] = fileparts(filename);
 [data, ~] = pop_loadbv(pname, [fname ext]);
+time = (1:length(data.data)) / data.srate;
 
 % find the chan numbers for C3, C4, and Cz, bicep, and tricep
 c3_chan_num = []; c4_chan_num = []; cz_chan_num = []; bicep_chan_num = []; tricep_chan_num = [];
@@ -62,9 +63,66 @@ high = Setup.EMGcutOffHighHz / (data.srate/2);
 
 low = Setup.EEGcutOffLowHz / (data.srate/2);
 high = Setup.EEGcutOffHighHz / (data.srate/2);
-[B_eeg, A_eeg] = butter(5, [low, high], 'bandpass' );
+[B_eeg, A_eeg] = butter(4, [low, high], 'bandpass' );
 
 % filter the data
-c3_filt = filtfilt(B_eeg, A_eeg, double(data.data(c3_chan_num,:)));
+% c3_filt = filtfilt(B_eeg, A_eeg, double(data.data(c3_chan_num,:)));
+
+c3_hp = hpfilt(double(data.data(c3_chan_num,:))', 4, Setup.EEGcutOffLowHz, data.srate);
+c3_filt = lpfilt(c3_hp, 4, Setup.EEGcutOffHighHz, data.srate);
+
+c4_hp = hpfilt(double(data.data(c4_chan_num,:))', 4, Setup.EEGcutOffLowHz, data.srate);
+c4_filt = lpfilt(c4_hp, 4, Setup.EEGcutOffHighHz, data.srate);
+
+cz_hp = hpfilt(double(data.data(cz_chan_num,:))', 4, Setup.EEGcutOffLowHz, data.srate);
+cz_filt = lpfilt(cz_hp, 4, Setup.EEGcutOffHighHz, data.srate);
+
+c3_cz = c3_filt - cz_filt;
+c4_cz = c4_filt - cz_filt;
+
+bicep_hp = hpfilt(double(data.data(bicep_chan_num,:))', 4, Setup.EMGcutOffLowHz, data.srate);
+bicep_filt = lpfilt(bicep_hp, 4, Setup.EMGcutOffHighHz, data.srate);
+
+tricep_hp = hpfilt(double(data.data(tricep_chan_num,:))', 4, Setup.EMGcutOffLowHz, data.srate);
+tricep_filt = lpfilt(tricep_hp, 4, Setup.EMGcutOffHighHz, data.srate);
+
+% plot eeg
+figure
+subplot(4,1,1)
+plot(time, c3_filt)
+ylabel('C3')
+subplot(4,1,2)
+plot(time, c3_cz)
+ylabel('C3-Cz')
+subplot(4,1,3)
+plot(time, c4_filt)
+ylabel('C4')
+subplot(4,1,4)
+plot(time, c4_cz)
+ylabel('C4-Cz')
+xlabel('time')
+
+% plot emg
+figure
+h_ax1 = subplot(2,1,1);
+plot(time, bicep_filt)
+h_ax2 = subplot(2,1,2);
+plot(time, tricep_filt)
+
+% add event times to emg
+for e_cnt = 1:length(data.event)
+	if strcmpi(data.event(e_cnt).code, 'rest')
+		t = data.event(e_cnt).latency / data.srate;
+		line(h_ax1, [t t], h_ax1.YLim, 'Color', [0.9 0 0])
+		line(h_ax2, [t t], h_ax2.YLim, 'Color', [0.9 0 0])
+	elseif strcmpi(data.event(e_cnt).code, 'extend')
+		t = data.event(e_cnt).latency / data.srate;
+		line(h_ax1, [t t], h_ax1.YLim, 'Color', [0 0.9 0])
+		line(h_ax2, [t t], h_ax2.YLim, 'Color', [0 0.9 0])
+	end
+		
+end
+
+
 
 keyboard
